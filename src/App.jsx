cf6bbbfa407b669529bf48ac4614b587e04e9ca8
Daniel Quinlan <daniel.quinlan@iinet.net.au>
2024-08-13 16:50:50 +1000

    Added react element picker

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import Chicken from './Chicken'
 import Greeter from './Greeter'
 import Die from './die'
+import ListPicker from './ListPicker'
 
 function App() {
 
   return (
     <div>
+      <ListPicker values={[1, 2, 3]}/>
+      <ListPicker values={["a", "b", "c"]}/>
       {/* <Greeter person="Rosa" from="Colt"/>
       <Greeter person="Stewart" from="Rosa"/> */}
-      <Die numSides={20}></Die>
+      {/* <Die numSides={20}></Die>
       <Die numSides={6}></Die>
-      <Die ></Die>
+      <Die ></Die> */}
     </div>)
 }
 
